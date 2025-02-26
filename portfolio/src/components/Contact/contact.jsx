3fefbf6755af33db847a2509643236b893e19a9c
Matheus Brito <ultrapistol@gmail.com>
2025-02-26 14:49:09 -0300

    Implantação de sistema de envio de e-mail e Atualização de  responsividade

--- portfolio/src/components/Contact/contact.jsx
+++ portfolio/src/components/Contact/contact.jsx
@@ -1,12 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './contact.css';
 
 import email from '../../assets/email.png';
 import whatsapp from '../../assets/whatsapp.png';
 import github from '../../assets/GithubLogo.png';
 import linkedin from '../../assets/linkedinLogo.png';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_t7cs7g7', 'template_zoc70mh', form.current, {
+        publicKey: 'cyviKg-mDz0nJ0Vtr',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert("Email enviado!");
+          e.target.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
   const [emoji, setEmoji] = useState(':)');
 
   useEffect(() => {
@@ -21,9 +43,9 @@ const Contact = () => {
     <section id='ContactPage'>
       <h1 className='ContactTitle'>- Contate-me -</h1>
       <span className='contactDesc'>Se preferir, estarei à disposição nos canais abaixo. Ou, caso deseje, envie-me uma mensagem pelo formulário:</span>
-      <form className='contactForm'>
-        <input type='text' className='name' placeholder='Seu nome' />
-        <input type='text' className='email' placeholder='Seu Email' />
+      <form className='contactForm' ref={form} onSubmit={sendEmail}>
+        <input type='text' className='name' placeholder='Seu nome' name='from_name' />
+        <input type='text' className='email' placeholder='Seu Email' name='from_email' />
         <textarea className='msg' name='message' rows='5' placeholder='Sua mensagem'></textarea>
         <button type='submit' value='Send' className='submitBtn'>Enviar</button>
         <div className='links'>
--- portfolio/src/components/Navbar/navbar.jsx
+++ portfolio/src/components/Navbar/navbar.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';  // Corrigido: importando useState
 import './navbar.css';
 import logo from '../../assets/Logo.png';
 import contactImage from '../../assets/contact.png';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
+import hamburger from '../../assets/Hamburger.png';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false); // Definição do estado showMenu
+
   return (
     <nav className="navbar">
         <img src={logo} alt="Logo" className='logo' />
         <div className="desktopMenu">
-            <Link className="desktopMenuListItem">HOME</Link>
-            <Link className="desktopMenuListItem">SOBRE</Link>
-            <Link className="desktopMenuListItem">PROJETOS</Link>
-            <Link className="desktopMenuListItem">CONTATOS</Link>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">HOME</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-1} duration={500} className="desktopMenuListItem">SOBRE</Link>
+            <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">PROJETOS</Link>
+            <Link activeClass='active' to='ContactPage' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">CONTATO</Link>
         </div>
         <button className="desktopMenuBtn">
-            <img src={contactImage} alt="" className="desktopMenuImg" />Contate-me</button>
+            <img src={contactImage} alt="" className="desktopMenuImg" />
+        </button>
+
+        <img src={hamburger} alt="Menu" className='mobMenu' onClick={() => setShowMenu(!showMenu)} />
+        <div className="NavMenu" style={{ display: showMenu ? 'flex' : 'none' }}>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>HOME</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-1} duration={500} className="listItem" onClick={() => setShowMenu(false)}>SOBRE</Link>
+            <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>PROJETOS</Link>
+            <Link activeClass='active' to='ContactPage' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>CONTATO</Link>
+        </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
